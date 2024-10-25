b769838bd1e4dc9f77545409ad44fceacdb37fb9
kokakarova <kostadinkakarova@gmail.com>
2024-10-25 13:01:49 +0200

    change method to get all unfetched messages

--- src/main/java/com/karova/messaging_service/domain/message/repos/MessageRepository.java
+++ src/main/java/com/karova/messaging_service/domain/message/repos/MessageRepository.java
@@ -4,8 +4,10 @@ import com.karova.messaging_service.domain.message.models.Message;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface MessageRepository extends ListCrudRepository<Message, UUID> {
+    List<Message> findAllByFetchedFalse();
 }
