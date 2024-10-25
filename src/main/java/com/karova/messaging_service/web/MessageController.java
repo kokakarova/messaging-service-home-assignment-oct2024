1ab735afc7c22e825ee75a3d8e1d95cc402ed603
kokakarova <kostadinkakarova@gmail.com>
2024-10-25 11:28:30 +0200

    add POST endpoint

---
+++ src/main/java/com/karova/messaging_service/web/MessageController.java
@@ -0,0 +1,37 @@
+package com.karova.messaging_service.web;
+
+import com.karova.messaging_service.domain.message.models.Message;
+import com.karova.messaging_service.domain.message.services.MessageService;
+import com.karova.messaging_service.web.dtos.SaveMessageReqDto;
+import com.karova.messaging_service.web.dtos.SaveMessageResDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+@RequiredArgsConstructor
+public class MessageController {
+
+    private final MessageService messageService;
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+
+    @PostMapping("/newMessage")
+    public ResponseEntity<SaveMessageResDto> saveNewMessage(@RequestBody SaveMessageReqDto messageReq) {
+        Message savedMessage = messageService.createMessage(messageReq);
+        SaveMessageResDto response = SaveMessageResDto.toDto(
+                savedMessage.getMsgId(),
+                messageReq.content(),
+                messageReq.senderId(),
+                messageReq.receiverId(),
+                savedMessage.getSentAt());
+        return ResponseEntity.ok(response);
+    }
+
+}
