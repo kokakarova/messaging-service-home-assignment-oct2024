5e946422e19c53af18e449638cb64ffbb2610936
kokakarova <kostadinkakarova@gmail.com>
2024-10-25 11:28:43 +0200

    add create new message method

--- src/main/java/com/karova/messaging_service/domain/message/services/MessageService.java
+++ src/main/java/com/karova/messaging_service/domain/message/services/MessageService.java
@@ -1,7 +1,25 @@
 package com.karova.messaging_service.domain.message.services;
 
+import com.karova.messaging_service.domain.message.models.Message;
+import com.karova.messaging_service.domain.message.repos.MessageRepository;
+import com.karova.messaging_service.domain.msgrelation.services.MsgRelationService;
+import com.karova.messaging_service.web.dtos.SaveMessageReqDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
 public class MessageService {
+    private final MessageRepository messageRepository;
+    private final MsgRelationService msgRelationService;
+
+    public Message createMessage(SaveMessageReqDto message) {
+        Message newMessage = messageRepository.save(new Message(UUID.fromString("2f3197d6-f0d9-480a-9784-2588012e3e73"),
+                message.content(), LocalDateTime.now()));
+        msgRelationService.createNewMessageRelations(message, newMessage);
+        return newMessage;
+    }
 }
