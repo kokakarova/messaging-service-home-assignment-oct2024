3a9f8305bb7d44b38ced1e6b0e27c3f9d02138e4
kokakarova <kostadinkakarova@gmail.com>
2024-10-25 11:29:07 +0200

    add getUserById method in MsgUserService

--- src/main/java/com/karova/messaging_service/domain/user/services/MsgUserService.java
+++ src/main/java/com/karova/messaging_service/domain/user/services/MsgUserService.java
@@ -1,7 +1,19 @@
 package com.karova.messaging_service.domain.user.services;
 
+import com.karova.messaging_service.domain.user.models.MsgUser;
+import com.karova.messaging_service.domain.user.repos.MsgUserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
 public class MsgUserService {
+    private final MsgUserRepository msgUserRepository;
+
+    public MsgUser getUserById(UUID id) {
+        return msgUserRepository.findById(id).orElse(null);
+    }
+
 }
