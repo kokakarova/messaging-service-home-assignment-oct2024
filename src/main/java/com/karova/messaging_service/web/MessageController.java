9d8280459097efe5fa4f3e66e11959e2817ed325
kokakarova <kostadinkakarova@gmail.com>
2024-10-25 13:00:43 +0200

    add get endpoint for all new messages

--- src/main/java/com/karova/messaging_service/web/MessageController.java
+++ src/main/java/com/karova/messaging_service/web/MessageController.java
@@ -6,8 +6,12 @@ import com.karova.messaging_service.web.dtos.SaveMessageReqDto;
 import com.karova.messaging_service.web.dtos.SaveMessageResDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.internal.constraintvalidators.bv.NullValidator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
@@ -21,8 +25,8 @@ public class MessageController {
     public String hello() {
         return "Hello World";
     }
-
-    @PostMapping("/newMessage")
+    // todo: explain and support why matching users by id (and not email, user_name etc.)
+    @PostMapping("/newMessages")
     public ResponseEntity<SaveMessageResDto> saveNewMessage(@Valid @RequestBody SaveMessageReqDto messageReq) {
         Message savedMessage = messageService.createMessage(messageReq);
         SaveMessageResDto response = SaveMessageResDto.toDto(
@@ -34,4 +38,16 @@ public class MessageController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping(value = {"/newMessages", "/newMessages/{id}"})
+    public ResponseEntity<List<Message>> getNewMessages(@PathVariable(value = "id", required = false) String userId) {
+        if (userId == null) {
+            // fetch all new messages
+            List<Message> allNewMessages = messageService.getAllNewMessages();
+            return ResponseEntity.ok(allNewMessages);
+        } else if (MsgValidator.isValid(userId)) {
+            // fetch new messages only for userId
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(List.of(new Message()));
+    }
 }
--- src/test/java/com/karova/messaging_service/web/MessageControllerTest.java
+++ src/test/java/com/karova/messaging_service/web/MessageControllerTest.java
@@ -18,7 +18,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -86,7 +85,7 @@ class MessageControllerTest {
     void shouldReturn_MockMessageId_WhenStatus_200OK() {
         SaveMessageReqDto mockValidReqBody = new SaveMessageReqDto(VALID_USER_ID_1, VALID_USER_ID_2, MOCK_MESSAGE_CONTENT);
         when(messageService.createMessage(mockValidReqBody))
-                .thenReturn(new Message(MOCK_MESSAGE_ID, MOCK_MESSAGE_CONTENT, LocalDateTime.now()));
+                .thenReturn(new Message(MOCK_MESSAGE_ID, MOCK_MESSAGE_CONTENT, LocalDateTime.now(), false));
         String json = objectMapper.writeValueAsString(mockValidReqBody);
         String url = UriComponentsBuilder.fromUriString(BASE_URL + "/newMessage")
                 .toUriString();
