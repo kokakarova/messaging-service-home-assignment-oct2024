36dc2e65482aaf1072b13a0dfb584cf7f165abe0
kokakarova <kostadinkakarova@gmail.com>
2024-10-28 11:16:23 +0100

    refactor long line

--- src/main/java/com/karova/messaging_service/domain/message/dtos/FetchNewMessagesDto.java
+++ src/main/java/com/karova/messaging_service/domain/message/dtos/FetchNewMessagesDto.java
@@ -3,5 +3,10 @@ package com.karova.messaging_service.domain.message.dtos;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record FetchNewMessagesDto(String messageId, String content, UUID senderId, UUID receiverId, LocalDateTime sentAt) {
+public record FetchNewMessagesDto(
+        String messageId,
+        String content,
+        UUID senderId,
+        UUID receiverId,
+        LocalDateTime sentAt) {
 }
